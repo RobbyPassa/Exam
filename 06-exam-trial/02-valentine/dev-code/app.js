e634b9058f13fe3306418e8b0603d26e7aa466aa
Roberto <roberto.passatempi@studenti.unitn.it>
2015-11-04 12:19:21 +0100

    added something

--- 06-exam-trial/02-valentine/dev-code/app.js
+++ 06-exam-trial/02-valentine/dev-code/app.js
@@ -1,7 +1,24 @@
 /* your code should go here */
 
+var tmpl ="<li>" + 
+          "<img src='image_link'>"+
+          "<h2>testo</h2>" +
+          "<button class='opt-choose' value='tema'>Choose</button></li>"
+
+
 $(document).ready(function(){
+    
+    for (var i = 0; i < data.length; i++) {
+        var add_to_ul = tmpl.replace("'image_link'", data[i].img)                                 
+                            .replace("'testo'", data[i].name)
+                            .replace("'tema'", data[i].id);
+        $(".themes").append(add_to_ul);
+    }
   
+    $('.opt-choose').on('click', function(){
+        $('#preview').toggleClass("done hidden");
+    })
+
   
 });
 
